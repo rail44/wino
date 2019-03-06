4ed1d1487a9bd412c4c612296bab984a67fe6d4d
Satoshi Amemiya <amemiya@protonmail.com>
2019-03-06 22:29:11 +0900

    rust 2018

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,20 +1,5 @@
 #![feature(proc_macro_hygiene)]
 
-extern crate atom_syndication;
-extern crate chrono;
-extern crate console_error_panic_hook;
-extern crate futures;
-extern crate js_sys;
-extern crate rss;
-extern crate serde;
-extern crate serde_json;
-extern crate squark;
-extern crate squark_macros;
-extern crate squark_web;
-extern crate wasm_bindgen;
-extern crate wasm_bindgen_futures;
-extern crate web_sys;
-
 use atom_syndication::Feed as AtomFeed;
 use console_error_panic_hook::set_once as set_panic_hook;
 use futures::Future;
